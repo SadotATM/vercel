aa4ddc711ed7a0fc92edf24db9a704a0ad31a42e
SadotATM <sadotantoniotorrejon@gmail.com>
2026-05-29 21:50:50 -0400

    5

--- app/pruebas/page.tsx
+++ app/pruebas/page.tsx
@@ -1,21 +1,14 @@
 "use client";
-import { Button } from "@/components/ui/button";
+
 import { motion } from "framer-motion";
 
 export default function PruebasPage() {
     return (
         <main className="p-10">
-            <h1 className="text-4xl font-bold">
-                Página de Pruebas
-            </h1>
-            <Button>Haz clic aquí</Button>
-
             <motion.div
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1 }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
             >
-
                 Hola mundo
             </motion.div>
         </main>
