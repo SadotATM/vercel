b7e747ee03de0e642ef302b940e521b5d069acf5
SadotATM <sadotantoniotorrejon@gmail.com>
2026-05-29 22:22:14 -0400

    8

--- app/pruebas/page.tsx
+++ app/pruebas/page.tsx
@@ -1,7 +1,15 @@
-export default function PruebasPage() {
+"use client";
+import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
+
+export default function AnimatedCard() {
     return (
-        <main>
-            Hola mundo
-        </main>
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+        >
+            Hola
+            <Button>Click me</Button>
+        </motion.div>
     );
 }
