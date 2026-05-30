e44a7d60ec32f5d3ecc4d2795a5b91ede1971958
SadotATM <sadotantoniotorrejon@gmail.com>
2026-05-29 22:36:45 -0400

    varios elementos

--- app/pruebas/page.tsx
+++ app/pruebas/page.tsx
@@ -1,16 +1,45 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { motion } from "framer-motion";
+import { ShowerHead } from "lucide-react";
+
+const container = {
+    hidden: {},
+    show: {
+        transition: {
+            staggerChildren: 0.2,
+        },
+    },
+};
+
+
+const item = {
+    hidden: { opacity: 0, y: 30 },
+    show: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+}
+
 
 export default function AnimatedCard() {
     return (
         <motion.div
             className="h-screen flex flex-col items-center justify-center gap-4"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
+            variants={container}
+            initial="hidden"
+            animate="show"
         >
-            Hola
-            <Button>Click me</Button>
+            <motion.h1 variants={item}>Hola</motion.h1>
+
+            <motion.p variants={item}>
+                Somos ASD
+            </motion.p>
+
+            <motion.div variants={item}>
+                <Button>Ver servicios</Button>
+            </motion.div>
+
+            <motion.div variants={item}>
+                <Button>Contacto</Button>
+            </motion.div>
         </motion.div>
     );
 }
