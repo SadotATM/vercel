cd6f1952af456bb93d1f7e89e37287f0cd523554
SadotATM <sadotantoniotorrejon@gmail.com>
2026-05-29 21:54:54 -0400

    6

--- app/pruebas/page.tsx
+++ app/pruebas/page.tsx
@@ -1,16 +1,7 @@
-"use client";
-
-import { motion } from "framer-motion";
-
 export default function PruebasPage() {
     return (
-        <main className="p-10">
-            <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-            >
-                Hola mundo
-            </motion.div>
+        <main>
+            Hola mundo
         </main>
     );
 }
